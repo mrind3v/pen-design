7cfebf477c41de009694c0cc5b690f75d7ad86fa
mrind3v <mrinmaydev@outlook.com>
2026-03-27 16:03:43 +0530

    wrote the Pen class and one of its implementation - BallPen. But i realised, most of the code in the concrete implementations BallPen and GellPen would be repeating violating DRY princple and also open close principle in the future if we want to check the write() method logic

--- src/BallPen.java
+++ src/BallPen.java
@@ -1,2 +1,16 @@
 public class BallPen extends Pen{
+    @Override
+    public void write() {
+        if (!isOpen) {
+            System.out.println("Pen is closed. Opening the cap...");
+            open();
+        }
+        if (refill==null || refill.getInkLevel()==0) {
+            System.out.println("Pen is out of ink or no refill found. Refilling...");
+            // assuming a default refill type
+            setRefill(new Refill("BLUE", InkType.BALL, 100));
+        }
+        System.out.println("Writing with blue pen...");
+        refill.consumeInk(5);
+    }
 }
--- src/Pen.java
+++ src/Pen.java
@@ -1,3 +1,28 @@
-abstract class Pen {
+abstract class Pen implements Observer{
+    protected Refill refill;
+    protected boolean isOpen = false;
 
+    @Override
+    public void update(int inkLevel) {
+        if (inkLevel==0) {
+            System.out.println("Pen is out of ink...");
+        } else {
+            System.out.println("Ink level updated" + inkLevel);
+        }
+    }
+
+    public void open() {
+        isOpen = true;
+    }
+
+    public void close() {
+        isOpen = false;
+    }
+
+    public void setRefill(Refill refill) {
+        this.refill = refill;
+        refill.registerObserver(this);
+    }
+
+    public abstract void write();
 }
